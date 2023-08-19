b4034d1c745563ad069d405ae19b1502d2ece43f
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-08-19 19:11:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ const equalizerBars = document.querySelectorAll(".bar");
 // Function to update the equalizer bars based on audio input
 function updateEqualizer(amplitude) {
   const scaledAmplitude = amplitude * 100 + 100; // Scale amplitude to control bar height
+  equalizerBars.forEach((bar, index) => {
