cfa327e0d70b098b9b228a523cebf7f69504749b
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-08-19 19:13:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,3 +12,4 @@ function updateEqualizer(amplitude) {
 function simulateVoiceInput() {
   const maxAmplitude = 0.5; // Adjust this value based on the input sensitivity
   const randomAmplitude = Math.random() * maxAmplitude;
+  updateEqualizer(randomAmplitude);
