1c64d46045fb59827cab9eb9f8df9d9c619c0b1f
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-08-19 19:11:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ const equalizerBars = document.querySelectorAll(".bar");
 function updateEqualizer(amplitude) {
   const scaledAmplitude = amplitude * 100 + 100; // Scale amplitude to control bar height
   equalizerBars.forEach((bar, index) => {
+    bar.style.height = `${scaledAmplitude - index * 20}px`; // Decrease bar height for a cascading effect
